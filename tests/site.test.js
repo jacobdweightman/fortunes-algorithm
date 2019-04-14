c18f99cdf11660ba0c52d9c091920e89a4ecaebe
Jacob Weightman <jacobdweightman@gmail.com>
2019-04-14 15:29:11 -0500

    added test for site event processing

--- tests/site.test.js
+++ tests/site.test.js
@@ -1,5 +1,15 @@
+const BeachLineTree = require('../js/BeachLineTree.js');
 const Site = require('../js/site.js');
 
 test('Site events create the proper vertex events', () => {
+  let blt = new BeachLineTree();
+
+  let a = new Site(100, 100);
+  let b = new Site(400, 100);
+  let c = new Site(300, 200);
+
+  a.process(blt);
+  
+
   expect(0).toBe(1);
 });
